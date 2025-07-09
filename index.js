de33e7d1c85d45f133e6ae201755cab408f777f3
wandoSilva <wandosilva94@gmail.com>
2025-07-09 15:14:30 -0300

    alterada a mensagem de saida

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+
 let nome
 let xp
+let nivel
 
 if(xp < 1000)  {
-    nome = "Ferro"
+    nivel = "Ferro"
 }else if (xp >= 1000 && xp <= 2000) {
-    nome = "Bronze"
+    nivel = "Bronze"
 }else if (xp >= 2001 && xp <= 5000){
-    nome = "Prata"
+    nivel = "Prata"
 }else if (xp >= 5001 && xp <= 7000){
-    nome = "Ouro"
+    nivel = "Ouro"
 }else if (xp >= 7001 && xp <= 8000){
-    nome = "Platina"
+    nivel = "Platina"
 }else if (xp >= 8001 && xp <= 9000){
-    nome = "Ascendente"
+    nivel = "Ascendente"
 }else if (xp >= 9001 && xp <= 10000){
-    nome = "Imortal"
+    nivel = "Imortal"
 }else {
     // xp > 10001:
-    nome = "Radiante"
+    nivel = "Radiante"
 }
 
-console.log(nome)
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
